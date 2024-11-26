7197ccece6390b4426e2dacb6552f9ffb4ac5d3a
Michael Medhat <126262769+michaelmedhat20@users.noreply.github.com>
2024-11-26 22:22:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Mock data
 const students = {
-    "273": { code: "30502061301194", grades: "50/50" },
-    "124": { code: "pass124", grades: "B, C, A" }
+    "273": {name:"مايكل مدحت يعقوب", code: "30502061301194", grades: "50/50" },
+    "124": { name: "مايكل مدحت يعقوب", code: "pass124", grades: "B, C, A" }
 };
 
 document.getElementById("gradeForm").addEventListener("submit", (event) => {
@@ -13,7 +13,7 @@ document.getElementById("gradeForm").addEventListener("submit", (event) => {
     const student = students[studentID];
 
     if (student && student.code === userCode) {
-        document.getElementById("result").innerText = `نتيجة الامتحان: ${student.grades}`;
+        document.getElementById("result").innerText = ` الاسم: ${student.name} \nنتيجة الامتحان : ${student.grades}`;
     } else {
         document.getElementById("result").innerText = "البيانات غير صحيحة";
     }
